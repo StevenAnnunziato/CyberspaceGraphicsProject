86ca764e4a6f7ceace5817f0d47b34138c58bbef
Steven Annunziato <steven.annunziato@mymail.champlain.edu>
2022-04-28 16:54:57 -0400

    Initial commit. Added files from GPR300Assignment2.

---
+++ Source/CyberspaceGame/CyberspaceGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CyberspaceGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERSPACEGAME_API ACyberspaceGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
